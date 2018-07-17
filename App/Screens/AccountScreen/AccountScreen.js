912148089feaae9a8b548217744b3698955ca2b3
Rosalio <xiaochendai@Xiaochens-MacBook-Pro.local>
2018-07-17 01:52:17 -0700

    Logout function

--- App/Screens/AccountScreen/AccountScreen.js
+++ App/Screens/AccountScreen/AccountScreen.js
@@ -1,19 +1,39 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+
+import { ButtonDark } from 'App/Components';
+import { LoginController } from 'App/Controllers';
+
+import NavigationRedux from 'App/Redux/Navigation/NavigationRedux';
+import store from 'App/Redux';
 
 class AccountScreen extends Component {
 
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start' }}>
-        <View>
-          <Text>This is the Account Screen!!!</Text>
-        </View>
+        <ButtonDark
+          title = 'Log out'
+          onPress={() => {
+            LoginController.logout().then(() => {
+              // TODO: migrate to use NavigationController
+              store.dispatch(NavigationRedux.changeRoot('login'));
+            });
+          }}
+        />
       </View>
     );
   }
 }
 
-export default AccountScreen;
+const mapStateToProps = ({ login }) => {
+  return {
+    country: login.country,
+    username: login.username,
+    user: login.user,
+  };
+};
+
+export default connect(mapStateToProps)(AccountScreen);
